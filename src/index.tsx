23743a02109a9c75a4c9f1b445a95c410c716d62
Edu-Kata-Academy <kata.academy.edu@gmail.com>
2024-04-03 12:56:52 +0300

    add solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// решение 1.1.6
+const element = React.createElement(
+  "h3",
+  null,
+  "Люк, я\u00A0твой\u00A0отец!\u00A0\u00A9"
+);
+root.render(element);
+
